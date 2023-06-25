a448ad84185f8b6fb52693df6b21df3134c63856
Burakherdogan <erdoganburakhasan@gmail.com>
2023-06-25 05:48:35 +0300

    create models

---
+++ restaurant_management/takeorder/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TakeorderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'takeorder'
---
+++ restaurant_management/takeorder/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Tables(models.Model):
+    number_of_table = models.IntegerField()
