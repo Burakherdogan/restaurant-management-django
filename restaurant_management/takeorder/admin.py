1276f58f0c531431a0941080b6114c81321993a2
Burakherdogan <erdoganburakhasan@gmail.com>
2023-12-07 20:58:37 +0300

    update

--- restaurant_management/takeorder/admin.py
+++ restaurant_management/takeorder/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Tables, Category
+from .models import Tables, Category, MenuItem, OrderModel
 admin.site.register(Tables)
 admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(OrderModel)
+
--- restaurant_management/takeorder/models.py
+++ restaurant_management/takeorder/models.py
@@ -7,7 +7,21 @@ class Tables(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    number = models.IntegerField()    
-    
     def __str__(self):
         return self.name
+    
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField('Category', related_name='item')
+    def __str__(self):
+        return self.name
+    
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
